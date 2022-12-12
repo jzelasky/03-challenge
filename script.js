7b1cd5bf3b1f62d278b3c564b638c213dd6d2687
Jessica Zelasky <jzelasky@gmail.com>
2022-12-12 09:12:53 -0500

    cleaned up comments

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-/*function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-*/
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-//to do: add error selecting no character types
-
 var generateBtn = document.querySelector("#generate");
 
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
